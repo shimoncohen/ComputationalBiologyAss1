e810ebdcc66c020e5d55585ee136553b7a49360a
shimoncohen <shimoncohen655@gmail.com>
2023-04-03 00:44:11 +0300

    chore: use IntEnum and calculate rumor prob only if has passing neighbour

--- src/person.py
+++ src/person.py
@@ -1,13 +1,18 @@
-from enum import Enum
-from random import random
-
+from enum import IntEnum
+import random
 import numpy as np
 
-class DoubtLevel(Enum):
+class DoubtLevel(IntEnum):
     S4 = 0,
     S3 = 1,
     S2 = 2,
     S1 = 3
+
+    # def __index__(self):
+    #     return self.value[0]
+
+    # def __int__(self):
+    #     return self.value[0]
 
     def get_probability(doubt_level: int) -> float:
         size = len(DoubtLevel)
@@ -21,19 +26,18 @@ class Person:
         self.cooldown = 0
 
     def should_pass_rumor(self, num_neighbours: int) -> bool:
-        doubt_level = self.doubt_level
-
-        if num_neighbours >= 2:
-            doubt_level -= 1
-        
         p = random.uniform(0, 1)
-        if p == 0:
+        if num_neighbours == 0 or self.cooldown > 0 or p == 0:
             return False
+
+        doubt_level = self.doubt_level
+        if num_neighbours >= 2 and doubt_level > 0:
+            doubt_level -= 1
 
         threshold = DoubtLevel.get_probability(doubt_level)
         return threshold >= p
 
-    def _activate_cooldown(self):
+    def activate_cooldown(self):
         self.cooldown = self.cooldown_time
 
     def update_cooldown(self):
