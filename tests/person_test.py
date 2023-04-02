97cd43a2ad006d367e53bfc8f6a2c9d8f9eed2ba
shimoncohen <shimoncohen655@gmail.com>
2023-04-03 02:12:08 +0300

    test: add tests for person

---
+++ tests/person_test.py
@@ -0,0 +1,44 @@
+import pytest
+from src import person
+
+class TestPerson:
+    @pytest.mark.parametrize(
+        "doubt_level,cooldown,num_neighbours,ans_after_cooldown",
+        [
+            pytest.param(
+                person.DoubtLevel.S1, 10, 1, True
+            ),
+            pytest.param(
+                person.DoubtLevel.S3, 5, 2, False
+            ),
+            pytest.param(
+                person.DoubtLevel.S4, 7, 1, False
+            ),
+        ],
+    )
+    def test_should_pass_rumor_with_cooldown(self, doubt_level, cooldown, num_neighbours, ans_after_cooldown):
+        p = person.Person(doubt_level, cooldown)
+        p.activate_cooldown()
+
+        for _ in range(cooldown):
+            assert p.should_pass_rumor(num_neighbours) == False
+            p.update_cooldown()
+        assert p.should_pass_rumor(num_neighbours) == ans_after_cooldown
+    
+    @pytest.mark.parametrize(
+        "doubt_level,num_neighbours,ans",
+        [
+            pytest.param(
+                person.DoubtLevel.S1, 1, True
+            ),
+            pytest.param(
+                person.DoubtLevel.S3, 5, False
+            ),
+            pytest.param(
+                person.DoubtLevel.S4, 8, False
+            ),
+        ],
+    )
+    def test_should_pass_rumor(self, doubt_level, num_neighbours, ans):
+        p = person.Person(doubt_level, 0)
+        assert p.should_pass_rumor(num_neighbours) == ans
