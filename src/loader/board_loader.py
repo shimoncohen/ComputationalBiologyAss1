ab8f3e2659a08f5f18dacc6fe822d7fb58437aae
shimoncohen <shimoncohen655@gmail.com>
2023-04-04 01:30:44 +0300

    feat: add board loader from file

---
+++ src/loader/board_loader.py
@@ -0,0 +1,15 @@
+import numpy as np
+from backend.person import Person
+
+class BoardLoader:
+    def Load(path: str) -> tuple[np.array, np.array, bool]:
+        with open(path) as f:
+            row, col = f.readline().split()
+            cooldown = int(f.readline())
+            people = np.array([[Person(int(doubt_level) - 1, cooldown) for doubt_level in line.strip()] for line in f.readlines()])
+
+        rows, cols = people.shape
+        rumor_board = np.full((rows, cols), False)
+        rumor_board[int(row), int(col)] = True
+
+        return rumor_board, people
