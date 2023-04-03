61d3f996f97146820a6954a2097ea0dcd85d1105
shimoncohen <shimoncohen655@gmail.com>
2023-04-04 01:31:57 +0300

    chore: add new load options to main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from backend.board import Board
-
+from loader.board_loader import BoardLoader
 
 if __name__ == "__main__":
     doubt_probs = [0.1, 0.1, 0.1, 0.7]
-    board = Board(5, 5, 10, 1, False, doubt_probs)
+    board = Board(False, 10)
+    board.initialize(5, 5, 1, doubt_probs)
+    # board.load('src/config/board.txt')
     board.print()
     for _ in range(5):  
         board.run_once()
