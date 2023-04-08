d64d0e32db42a269a952c49fcb48cdc4cd3920d7
Lee Shoshani <lital.shoshani@gmail.com>
2023-04-08 22:59:17 +0300

    Organize colors in one file

--- src/ui/colors.py
+++ src/ui/colors.py
@@ -8,6 +8,11 @@ GREEN_COLOR = pg.color.Color('#00FF00')
 BLUE_COLOR = pg.color.Color('#0000FF')
 YELLOW_COLOR = pg.color.Color('#FFFF00')
 
+COLOR_INACTIVE = pg.Color('lightskyblue3')
+COLOR_ACTIVE = pg.Color('dodgerblue2')
+
+COLOR_LIST_INACTIVE = (255, 100, 100)
+COLOR_LIST_ACTIVE = (255, 150, 150)
 
 def color_from_hex(hex: str) -> pg.color.Color:
     return pg.color.Color(hex)
