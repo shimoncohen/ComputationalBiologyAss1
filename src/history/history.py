513c0e7737b3c8fe9120ee3d8b5d8b114533dbf8
shimoncohen <shimoncohen655@gmail.com>
2023-04-18 02:20:03 +0300

    chore(history): create parent directories and save plots to buffer

--- src/history/history.py
+++ src/history/history.py
@@ -1,4 +1,5 @@
-import os
+import io
+from pathlib import Path
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
@@ -19,6 +20,7 @@ class History(Generic[T]):
         return [self.__stack[0].get_csv_header_row(), *[item.get_as_csv_row() for item in self.__stack]]
     
     def save(self, path: str) -> None:
+        # TODO: move logic to somewhere specific
         csv_lines = StringIO('\n'.join(self.get_history_csv()))
         df = pd.read_csv(csv_lines, header='infer')
         df['total_rumor_percentage'] = df['total_rumors'].values / df['total_people'].values
@@ -29,22 +31,28 @@ class History(Generic[T]):
         df.plot(kind='line', y='s2_rumors', ax=ax)
         df.plot(kind='line', y='s3_rumors', ax=ax)
         df.plot(kind='line', y='s4_rumors', ax=ax)
-        plt.savefig('count.png')
+        
+        # From: https://stackoverflow.com/questions/8598673/how-to-save-a-pylab-figure-into-in-memory-file-which-can-be-read-into-pil-image
+        buffer1 = io.BytesIO()
+        plt.savefig(buffer1, format='png')
         plt.cla()
 
         ax.yaxis.set_major_formatter(mtick.PercentFormatter())
         df.plot(kind='line', xlabel='generation', y='total_rumor_percentage', ax=ax)
-        plt.savefig('percentage.png')
+        
+        buffer2 = io.BytesIO()
+        plt.savefig(buffer2, format='png')
+        plt.cla()
 
+        # Create parent directories
+        Path(path).parent.mkdir(parents=True, exist_ok=True, mode=666)
         writer = pd.ExcelWriter(path, engine='xlsxwriter')
         df.to_excel(writer, sheet_name='Data')
         worksheet = writer.sheets['Data']
-        worksheet.insert_image('N1', 'count.png')
-        worksheet.insert_image('N25', 'percentage.png')
+        # From: https://xlsxwriter.readthedocs.io/worksheet.html#insert_image
+        worksheet.insert_image('N1', 'image.png', {'image_data': buffer1})
+        worksheet.insert_image('N25', 'image.png', {'image_data': buffer2})
         writer.close()
-
-        os.remove('count.png')
-        os.remove('percentage.png')
 
 
     def __getitem__(self, item):
